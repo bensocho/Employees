252153074bc0e10c4ec15918e6ecb14bf3ec4b52
bensocho <bensocho10@gmail.com>
2023-04-23 14:24:06 +0300

    client request

--- Server.py
+++ Server.py
@@ -1,1 +1,11 @@
+from flask import Flask
+import requests
 
+app = Flask(__name__)
+
+@app.route("/test", methods=["GET"])
+def health_check():
+    return "healthy"
+
+if __name__ == "__main__":
+    app.run(debug=True)
